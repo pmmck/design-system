3aa9bc8f64ca26e89be9577368f1a5e8050bbb18
Rachel Vasquez <rachel.vasquez@10up.com>
2016-05-11 16:19:02 -0400

    working on cleaning up JS

--- src/assets/fabricator/scripts/fabricator.js
+++ src/assets/fabricator/scripts/fabricator.js
@@ -1,320 +1,277 @@
-'use strict';
+( function( fabricator ) {
+	'use strict';
 
-require('./prism');
-
-/**
- * Global `fabricator` object
- * @namespace
- */
-var fabricator = window.fabricator = {};
-
-
-/**
- * Default options
- * @type {Object}
- */
-fabricator.options = {
-	menu: false,
-	mq: '(min-width: 60em)'
-};
-
-// open menu by default if large screen
-fabricator.options.menu = window.matchMedia(fabricator.options.mq).matches;
-
-/**
- * Feature detection
- * @type {Object}
- */
-fabricator.test = {};
-
-// test for sessionStorage
-fabricator.test.sessionStorage = (function () {
-	var test = '_f';
-	try {
-		sessionStorage.setItem(test, test);
-		sessionStorage.removeItem(test);
-		return true;
-	} catch(e) {
-		return false;
-	}
-}());
-
-// create storage object if it doesn't exist; store options
-if (fabricator.test.sessionStorage) {
-	sessionStorage.fabricator = sessionStorage.fabricator || JSON.stringify(fabricator.options);
-}
-
-
-/**
- * Cache DOM
- * @type {Object}
- */
-fabricator.dom = {
-	root: document.querySelector('html'),
-	primaryMenu: document.querySelector('.f-menu'),
-	menuItems: document.querySelectorAll('.f-menu li a'),
-	menuToggle: document.querySelector('.f-menu-toggle')
-};
-
-
-/**
- * Get current option values from session storage
- * @return {Object}
- */
-fabricator.getOptions = function () {
-	return (fabricator.test.sessionStorage) ? JSON.parse(sessionStorage.fabricator) : fabricator.options;
-};
-
-
-/**
- * Build color chips
- */
-fabricator.buildColorChips = function () {
-
-	var chips = document.querySelectorAll('.f-color-chip'),
-		color;
-
-	for (var i = chips.length - 1; i >= 0; i--) {
-		color = chips[i].querySelector('.f-color-chip__color').innerHTML;
-		chips[i].style.borderTopColor = color;
-		chips[i].style.borderBottomColor = color;
-	}
-
-	return this;
-
-};
-
-
-/**
- * Add `f-active` class to active menu item
- */
-fabricator.setActiveItem = function () {
+	require('./prism');
 
 	/**
-	 * Match the window location with the menu item, set menu item as active
+	 * Global `fabricator` object
+	 * @namespace
 	 */
-	var setActive = function () {
+	//var fabricator = window.fabricator = {};
+	var fabricator = function() {
 
-		// get current file and hash without first slash
-		var current = (window.location.pathname + window.location.hash).replace(/(^\/)([^#]+)?(#[\w\-\.]+)?$/ig, function (match, slash, file, hash) {
-				hash = hash || '';
-				file = file.replace( 'dist/', '' ).replace( 'design-system/', '' ) || '';
-				// Currently, without a scrolling listener, there's no way to
-				// change as we visit new 'hashes'. Better to leave at top
-				// level link
-				return './' + file; // + hash.split('.')[0];
-			}) || 'index.html',
-			href;
+		function initialize() {
+			var self = this;
 
-		// find the current section in the items array
-		for (var i = fabricator.dom.menuItems.length - 1; i >= 0; i--) {
+			/**
+			 * Default options
+			 * @type {Object}
+			 */
+			self.options = {
+				menu: false,
+				mq: '(min-width: 60em)'
+			};
 
-			var item = fabricator.dom.menuItems[i];
-			// get item href without first slash
-			href = item.getAttribute('href').replace(/^\//g, '');
+			// open menu by default if large screen
+			self.options.menu = window.matchMedia(self.options.mq).matches;
 
-			if ( href === current ) {
-				fabricator.addClass( item, 'current');
-			} else {
-				fabricator.removeClass( item, 'current');
+			/**
+			 * Feature detection
+			 * @type {Object}
+			 */
+			self.test = {};
+
+			// test for sessionStorage
+			self.test.sessionStorage = (function () {
+				var test = '_f';
+				try {
+					sessionStorage.setItem(test, test);
+					sessionStorage.removeItem(test);
+					return true;
+				} catch(e) {
+					return false;
+				}
+			}());
+
+			// create storage object if it doesn't exist; store options
+			if (self.test.sessionStorage) {
+				sessionStorage.self = sessionStorage.self || JSON.stringify(self.options);
+			}
+
+
+			/**
+			 * Cache DOM
+			 * @type {Object}
+			 */
+			self.dom = {
+				root: document.querySelector('html'),
+				primaryMenu: document.querySelector('.f-menu'),
+				menuItems: document.querySelectorAll('.f-menu li a'),
+				menuToggle: document.querySelector('.f-menu-toggle')
+			};
+
+
+			/**
+			 * Get current option values from session storage
+			 * @return {Object}
+			 */
+			self.getOptions = function () {
+				return (self.test.sessionStorage) ? JSON.parse(sessionStorage.self) : self.options;
+			};
+
+		}
+
+		/**
+		 * Build color chips
+		 */
+		function buildColorChips() {
+
+			var chips = document.querySelectorAll('.f-color-chip'),
+				color;
+
+			for (var i = chips.length - 1; i >= 0; i--) {
+				color = chips[i].querySelector('.f-color-chip__color').innerHTML;
+				chips[i].style.borderTopColor = color;
+				chips[i].style.borderBottomColor = color;
 			}
 		}
-	};
-
-	window.addEventListener('hashchange', setActive);
-
-	setActive();
-
-	return this;
-
-};
 
 
-/**
- * Click handler to primary menu toggle
- * @return {Object} fabricator
- */
-fabricator.menuToggle = function () {
+		/**
+		 * Add `f-active` class to active menu item
+		 */
+		function setActiveItem() {
 
-	// shortcut menu DOM
-	var toggle = fabricator.dom.menuToggle;
+			/**
+			 * Match the window location with the menu item, set menu item as active
+			 */
+			var setActive = function () {
 
-	var options = fabricator.getOptions();
+				// get current file and hash without first slash
+				var current = (window.location.pathname + window.location.hash).replace(/(^\/)([^#]+)?(#[\w\-\.]+)?$/ig, function (match, slash, file, hash) {
+						hash = hash || '';
+						file = file.replace( 'dist/', '' ).replace( 'design-system/', '' ) || '';
+						// Currently, without a scrolling listener, there's no way to
+						// change as we visit new 'hashes'. Better to leave at top
+						// level link
+						return './' + file; // + hash.split('.')[0];
+					}) || 'index.html',
+					href;
 
-	// toggle classes on certain elements
-	var toggleClasses = function () {
-		//TODO: Replace ClassList!
-		var menuClassList = fabricator.dom.root.className.split(' ');
-		options.menu = !fabricator.dom.root.classList.contains('f-menu-active');
-		fabricator.dom.root.classList.toggle('f-menu-active');
+				// find the current section in the items array
+				for (var i = self.dom.menuItems.length - 1; i >= 0; i--) {
 
-		if (fabricator.test.sessionStorage) {
-			sessionStorage.setItem('fabricator', JSON.stringify(options));
+					var item = self.dom.menuItems[i];
+					// get item href without first slash
+					href = item.getAttribute('href').replace(/^\//g, '');
+
+					if ( href === current ) {
+						fabricator.helpers.addClass( item, 'current');
+					} else {
+						fabricator.helpers.removeClass( item, 'current');
+					}
+				}
+			};
+
+			window.addEventListener('hashchange', setActive);
+
+			setActive();
 		}
-	};
-
-	// toggle classes on ctrl + m press
-	document.onkeydown = function (e) {
-		e = e || event
-		if (e.ctrlKey && e.keyCode == 'M'.charCodeAt(0)) {
-			toggleClasses();
-		}
-	}
-
-	// toggle classes on click
-	toggle.addEventListener('click', function () {
-		toggleClasses();
-	});
-
-	// close menu when clicking on item (for collapsed menu view)
-	var closeMenu = function () {
-		if (!window.matchMedia(fabricator.options.mq).matches) {
-			toggleClasses();
-		}
-	};
-
-	for (var i = 0; i < fabricator.dom.menuItems.length; i++) {
-		fabricator.dom.menuItems[i].addEventListener('click', closeMenu);
-	}
-
-	return this;
-
-};
-
-/**
- * Automatically select code when code block is clicked
- */
-fabricator.bindCodeAutoSelect = function () {
-
-	var codeBlocks = document.querySelectorAll('.f-item-code');
-
-	var select = function (block) {
-		var selection = window.getSelection();
-		var range = document.createRange();
-		range.selectNodeContents(block.querySelector('code'));
-		selection.removeAllRanges();
-		selection.addRange(range);
-	};
-
-	for (var i = codeBlocks.length - 1; i >= 0; i--) {
-		codeBlocks[i].addEventListener('click', select.bind(this, codeBlocks[i]));
-	}
-
-	return this;
-};
 
 
-/**
- * Open/Close menu based on session var.
- * Also attach a media query listener to close the menu when resizing to smaller screen.
- */
-fabricator.setInitialMenuState = function () {
+		/**
+		 * Click handler to primary menu toggle
+		 * @return {Object} fabricator
+		 */
+		function menuToggle() {
 
-	// root element
-	var root = document.querySelector('html');
+			// shortcut menu DOM
+			var toggle = fabricator.dom.menuToggle;
 
-	var mq = window.matchMedia(fabricator.options.mq);
+			var options = fabricator.getOptions();
 
-	// if small screen
-	var mediaChangeHandler = function (list) {
-		if (!list.matches) {
-			fabricator.removeClass( root, 'f-menu-active');
-		} else {
-			if (fabricator.getOptions().menu) {
-				fabricator.addClass( root, 'f-menu-active');
-			} else {
-				fabricator.removeClass( root, 'f-menu-active');
+			// toggle classes on certain elements
+			var toggleClasses = function () {
+				//TODO: Replace ClassList!
+				var menuClassList = self.dom.root.className.split(' ');
+				options.menu = !self.dom.root.classList.contains('f-menu-active');
+				self.dom.root.classList.toggle('f-menu-active');
+
+				if (self.test.sessionStorage) {
+					sessionStorage.setItem('fabricator', JSON.stringify(options));
+				}
+			};
+
+			// toggle classes on ctrl + m press
+			document.onkeydown = function (e) {
+				e = e || event
+				if (e.ctrlKey && e.keyCode == 'M'.charCodeAt(0)) {
+					toggleClasses();
+				}
+			}
+
+			// toggle classes on click
+			toggle.addEventListener('click', function () {
+				toggleClasses();
+			});
+
+			// close menu when clicking on item (for collapsed menu view)
+			var closeMenu = function () {
+				if (!window.matchMedia(self.options.mq).matches) {
+					toggleClasses();
+				}
+			};
+
+			for (var i = 0; i < self.dom.menuItems.length; i++) {
+				self.dom.menuItems[i].addEventListener('click', closeMenu);
 			}
 		}
-	};
 
-	mq.addListener(mediaChangeHandler);
-	mediaChangeHandler(mq);
+		/**
+		 * Automatically select code when code block is clicked
+		 */
+		function bindCodeAutoSelect() {
 
-	return this;
+			var codeBlocks = document.querySelectorAll('.f-item-code');
 
-};
+			var select = function (block) {
+				var selection = window.getSelection();
+				var range = document.createRange();
+				range.selectNodeContents(block.querySelector('code'));
+				selection.removeAllRanges();
+				selection.addRange(range);
+			};
 
-/**
- * Add fixed class to sidebar on scroll
- */
-fabricator.fixSidebar = function () {
-	var dsHeaderTop  = document.querySelector( '.f-header-top' ),
-		dsHeader  = document.querySelector( '.f-header' ),
-		dsSidebar = document.querySelector( '.f-menu' ),
-		headerTopHeight = dsHeaderTop.offsetHeight,
-		headerHeight = dsHeader.offsetHeight,
-		totalHeaderHeight = headerTopHeight + headerHeight;
-
-
-	if ( 'undefined' === typeof dsHeaderTop || null === dsHeaderTop ) {
-		return;
-	}
-
-	if ( 'undefined' === typeof dsHeader || null === dsHeader ) {
-		return;
-	}
-
-	if ( 'undefined' === typeof dsSidebar || null === dsSidebar ) {
-		return;
-	}
-
-	window.onscroll = function() {
-		var topOffset = window.pageYOffset;
-
-		if ( window.pageYOffset > totalHeaderHeight ) {
-			fabricator.addClass( dsSidebar, 'fixed' );
-		} else {
-			fabricator.removeClass( dsSidebar, 'fixed' );
+			for (var i = codeBlocks.length - 1; i >= 0; i--) {
+				codeBlocks[i].addEventListener('click', select.bind(this, codeBlocks[i]));
+			}
 		}
-	};
 
-	return this;
 
-};
+		/**
+		 * Open/Close menu based on session var.
+		 * Also attach a media query listener to close the menu when resizing to smaller screen.
+		 */
+		function setInitialMenuState() {
 
-/**
- * Helper: Add Class to Element
- */
-fabricator.addClass = function ( el, className ) {
-	//Check if element is undefined or null first
-	if ( 'undefined' === typeof el || null === el ) {
-		return;
-	}
-	// So we don't have duplicates
-	fabricator.removeClass( el, className );
-	el.className += ' ' + className;
-};
+			// root element
+			var root = document.querySelector('html');
 
-/**
- * Helper: Remove Class from Element
- */
-fabricator.removeClass = function ( el, className ) {
-	//Check if element is undefined or null first
-	if ( 'undefined' === typeof el || null === el ) {
-		return;
-	}
+			var mq = window.matchMedia(self.options.mq);
 
-	//Go to end of index for existing classes and remove desired class
-	if ( el.className.indexOf( ' ' + className ) > -1 ) {
-		el.className = el.className.replace( ' ' + className , '' );
-	} else if ( el.className.indexOf( className ) > -1 ) {
-		el.className = el.className.replace( className , '' );
-	}
-};
+			// if small screen
+			var mediaChangeHandler = function (list) {
+				if (!list.matches) {
+					fabricator.helpers.removeClass( root, 'f-menu-active');
+				} else {
+					if (self.getOptions().menu) {
+						fabricator.helpers.addClass( root, 'f-menu-active');
+					} else {
+						fabricator.helpers.removeClass( root, 'f-menu-active');
+					}
+				}
+			};
 
-/**
- * Initialization
- */
-(function () {
+			mq.addListener(mediaChangeHandler);
+			mediaChangeHandler(mq);
+		}
 
-	// invoke
-	fabricator
-		.setInitialMenuState()
-		.menuToggle()
-		.buildColorChips()
-		.setActiveItem()
-		.bindCodeAutoSelect()
-		.fixSidebar();
+		/**
+		 * Add fixed class to sidebar on scroll
+		 */
+		function fixSidebar() {
+			var dsHeaderTop  = document.querySelector( '.f-header-top' ),
+				dsHeader  = document.querySelector( '.f-header' ),
+				dsSidebar = document.querySelector( '.f-menu' ),
+				headerTopHeight = dsHeaderTop.offsetHeight,
+				headerHeight = dsHeader.offsetHeight,
+				totalHeaderHeight = headerTopHeight + headerHeight;
 
-}());
+
+			if ( 'undefined' === typeof dsHeaderTop || null === dsHeaderTop ) {
+				return;
+			}
+
+			if ( 'undefined' === typeof dsHeader || null === dsHeader ) {
+				return;
+			}
+
+			if ( 'undefined' === typeof dsSidebar || null === dsSidebar ) {
+				return;
+			}
+
+			window.onscroll = function() {
+				var topOffset = window.pageYOffset;
+
+				if ( window.pageYOffset > totalHeaderHeight ) {
+					fabricator.helpers.addClass( dsSidebar, 'fixed' );
+				} else {
+					fabricator.helpers.removeClass( dsSidebar, 'fixed' );
+				}
+			};
+		}
+
+		return {
+			initialize : initialize,
+			setInitialMenuState : setInitialMenuState,
+			menuToggle : menuToggle,
+			buildColorChips : buildColorChips,
+			setActiveItem : setActiveItem,
+			bindCodeAutoSelect : bindCodeAutoSelect,
+			fixSidebar : fixSidebar,
+		};
+	}();
+
+	fabricator.initialize();
+
+})( fabricator );
---
+++ src/assets/fabricator/scripts/helpers.js
@@ -0,0 +1,41 @@
+( function( fabricator ) {
+
+	fabricator.helpers = function() {
+
+		/**
+		 * Helper to add a className to an element
+		 */
+		function addClass( el, className ) {
+			//Check if element is undefined or null first
+			if ( 'undefined' === typeof el || null === el ) {
+				return;
+			}
+			// So we don't have duplicates
+			removeClass( el, className );
+			el.className += ' ' + className;
+		}
+
+		/**
+		 * Helper to remove a className from an element
+		 */
+		function removeClass( el, className ) {
+			//Check if element is undefined or null first
+			if ( 'undefined' === typeof el || null === el ) {
+				return;
+			}
+
+			//Go to end of index for existing classes and remove desired class
+			if ( el.className.indexOf( ' ' + className ) > -1 ) {
+				el.className = el.className.replace( ' ' + className , '' );
+			} else if ( el.className.indexOf( className ) > -1 ) {
+				el.className = el.className.replace( className , '' );
+			}
+		}
+
+		return {
+			addClass    : addClass,
+			removeClass : removeClass,
+		};
+	}();
+
+})( fabricator );
